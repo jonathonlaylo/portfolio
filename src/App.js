38745479b408b0f4f4c439661e5eb206282a3fa7
jonathonlaylo <jonathon.laylo@gmail.com>
2018-04-10 01:24:48 -0700

    funny layout template

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          this is my portfolio. hire me. k thanks 🤙
         </p>
       </div>
     );
