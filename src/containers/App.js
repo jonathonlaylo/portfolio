3ed8d8ec4295e31c884e407d210868ca8f85a77c
jonathonlaylo <jonathon.laylo@gmail.com>
2018-04-11 00:09:07 -0700

    added header component and lineked with container App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.jpg';
+import Header from '../components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
+        <Header/>
         <p className="App-intro">
           this is my portfolio. hire me. k thanks 🤙
         </p>
